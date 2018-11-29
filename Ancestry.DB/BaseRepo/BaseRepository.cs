12d8ff85cd10af6da9cac508bee73838d5968dfb
x162335 <x162335@BCVRTEM0N101-V>
2018-11-29 05:36:43 -0600

    Add project files.

---
+++ Ancestry.DB/BaseRepo/BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ancestry.DB.BaseRepo
+{
+    public class BaseRepository
+    {
+        internal Database DbContect { get; set; }
+        public BaseRepository()
+        {
+            DbContect = new Database("Ancestry");
+        }
+
+        public int Insert<T>(T tableData)
+        {
+            int result = 0;
+            Convert.ToInt32(DbContect.Insert(tableData));
+            return result;
+        }
+
+        public List<T> Select<T>(string query)
+        {
+            List<T> outputList = null;
+            outputList =  DbContect.Fetch<T>(query).ToList();
+            return outputList;
+        }
+
+        public int Update<T>(T tableName, int Id)
+        {
+            int flag = 0;
+            flag = DbContext.Update(tableName, Id);
+            return flag;
+        }
+    }
+}
