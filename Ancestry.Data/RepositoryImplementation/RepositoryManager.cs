401f40c3313647940192e23c043b12e93aa0d97f
x162335 <x162335@BCVRTEM0N101-V>
2018-12-04 07:07:27 -0600

    Updated

--- Ancestry.Data/RepositoryImplementation/RepositoryManager.cs
+++ Ancestry.Data/RepositoryImplementation/RepositoryManager.cs
@@ -9,5 +9,6 @@ namespace Ancestry.Data.RepositoryImplementation
 {
     public class RepositoryManager : IRepositoryManager
     {
+
     }
 }
---
+++ Ancestry.Models/Models/UserInformation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ancestry.Models
+{
+    public class UserInformation : BaseModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string DOB { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Profession { get; set; }
+        public string HouseName { get; set; }
+        public int SpouseId { get; set; }
+        public int SiblingsId { get; set; }
+        public int ChildId { get; set; }
+
+        /*
+         * ID int IDENTITY(1,1) primary key,
+Name varchar(200),
+Sex varchar(50),
+DOB varchar(200),
+Mobile varchar(200),
+Email varchar(200),
+Profession varchar(200),
+HouseName varchar(200),
+SpouseId int,
+SiblingsId int,
+ChildId int
+         */
+    }
+}
