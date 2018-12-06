5882d18c2b7ee4400f90097205e5b33baf3a0682
x162335 <x162335@BCVRTEM0N101-V>
2018-12-06 08:09:53 -0600

    validation corrected.

--- Ancestry.Models/Models/LoginViewModel.cs
+++ Ancestry.Models/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Ancestry.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage ="User Name required")]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
--- Ancestry.Web/Controllers/AccountController.cs
+++ Ancestry.Web/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ancestry.Models;
+using Ancestry.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,36 @@ namespace Ancestry.Web.Controllers
 {
     public class AccountController : Controller
     {
+        public IAccountService accountService;
+
+        public AccountController()
+        {
+            accountService = new AccountService();
+        }
         // GET: Account
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserInformation info = null;
+                info = accountService.ValidateUserData(model);
+                if (info != null)
+                {
+                    return RedirectToAction("");
+                }
+            }
             return View();
         }
     }
